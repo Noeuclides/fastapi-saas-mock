fd324bb7c70d96c2d99d0ef86fa50987fb92cc6a
Nicolás Martínez <nicolas.martinez@symplifica.com>
2025-05-29 18:10:25 -0500

    adding endponints

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/auth/login")
+async def login():
+    return {"status": "ok", "token": "fake-token"}
+
+@app.get("/orders")
+async def read_orders():
+    return [{"order_id": 1}, {"order_id": 2}]
+
+@app.get("/orders/{order_id}")
+async def read_order(order_id: int, q: Optional[str] = None):
+    return {"order_id": order_id, "q": q}
+
+@app.get("/analytics")
+async def read_analytics():
+    return {"analytics": "data"}
+
